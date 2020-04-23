b94fb112ac00a0c7dc9f2fdd591abc16ca0bf71e
sairama <ram9151@gmail.com>
2020-04-23 13:00:06 +0530

    Delete forms.py

--- PYTHON/FLASK/myproject/forms.py
+++
@@ -1,8 +0,0 @@
-#owners
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-
-class AddForm(FlaskForm):
-    name = StringField('Name of owner:')
-    pup_id = IntegerField("id of puppy : ")
-    submit = SubmitField("add owner: ")
